d73c4d3d37aa8da97fa76ef841daebdb5543d636
thismars <yhklip990@gmail.com>
2023-08-03 17:45:09 +0900

    update: middleware to correctly log client IP

--- src/middlewares/logger.middleware.ts
+++ src/middlewares/logger.middleware.ts
@@ -13,13 +13,16 @@ export class LoggerMiddleware implements NestMiddleware {
   constructor(@Inject(Logger) private readonly logger: LoggerService) {}
 
   use(req: Request, res: Response, next: NextFunction) {
-    const { ip, method, originalUrl } = req;
+    const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+    const method = req.method;
+    const originalUrl = req.originalUrl;
     const userAgent = req.get('user-agent');
 
     res.on('finish', () => {
       const { statusCode } = res;
+      const timestamp = new Date().toISOString();
       this.logger.log(
-        `${method} ${originalUrl} ${statusCode} ${ip} ${userAgent}`,
+        `${method} ${originalUrl} ${statusCode} ${ip} ${userAgent} ${timestamp}`,
       );
     });
 
